b5381e7a8344fec2c24a65449d92989a4e4cd53c
Liashova Valeriia <lera.l0205@gmail.com>
2023-09-29 18:39:07 +0300

    Добавила задачу на секунды

--- time.pas
+++ time.pas
@@ -1,6 +1,6 @@
 ﻿begin
   var a:= readinteger('Введите год: ');
   if (a mod 4 = 0) and (a mod 100 = 0) and (a mod 400 = 0) then
-    print('Високосный год')
-  else print('Обычный год')
+    print('Високосный год, 366 дней' )
+  else print('Обычный год, 365 дней')
 end.
---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿##
+
+var hours := readinteger('Введите количество часов: ');
+var minutes := readinteger('Введите количество минут: ');
+var seconds := readinteger('Введите количество секунд: ');
+var all := hours * 60 * 60 + minutes * 60 + seconds;
+print(all)
